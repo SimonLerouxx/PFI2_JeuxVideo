2266a4821c68f176cd20961bf459233478c15d56
SimonLerouxx <95500169+SimonLerouxx@users.noreply.github.com>
2023-11-30 18:56:18 -0500

    dssdf

---
+++ PFI2_JeuxVideoo/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public float vitesseLook = 1.5f;
+    public float lookXLimit = 45.0f;
+    float rotationX;
+    public float sensitivity = 40;
+    float verticalRotation = 0;
+
+    [SerializeField] GameObject Player;
+    [SerializeField] GameObject Gun;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Rotation X
+        rotationX = Input.GetAxis("Mouse X") * vitesseLook;
+        Player.transform.Rotate(Vector3.up * rotationX);
+
+
+        //Rotation Y
+        Vector2 rotation = Mouse.current.delta.ReadValue() * Time.deltaTime * sensitivity;
+        verticalRotation = verticalRotation- rotation.y;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90, 90);
+        transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+
+    }
+
+}
+
+   
---
+++ PFI2_JeuxVideoo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+//Travail fait par SImon Leroux et Lucas Romanias
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    Vector3 deplacement;
+    public float speed = 6;
+    public float minSpeed = 6;
+    public float maxSpeed = 10;
+    [SerializeField] GameObject Camera;
+    [SerializeField] float jumpForce = 75;
+    Rigidbody rb;
+    bool canJump = false;
+
+
+
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(deplacement == Vector3.zero)
+        {
+            speed = minSpeed;
+        }
+        else
+        {
+            transform.Translate(deplacement * Time.deltaTime * speed, Space.Self);
+
+            if(speed < maxSpeed)
+            {
+                speed += 0.01f;
+            }
+        }
+        
+        
+        RaycastHit hit;
+        float thickness = 0.5f; //<-- Desired thickness here.
+        Vector3 origin = transform.position;
+        Vector3 direction = transform.TransformDirection(Vector3.down);
+        //if (Physics.SphereCast(origin, thickness, Vector3.down, out hit,0.2f))
+        //{
+        //    Debug.DrawLine(transform.position, hit.point, Color.red);
+        //    canJump = true;
+        //}
+        ////Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 0.85f) || Physics.Raycast(transform.position + new Vector3(0.5f, 0, 0), transform.TransformDirection(Vector3.down), out hit, 0.85f) ||
+             Physics.Raycast(transform.position + new Vector3(-0.5f, 0, 0f), transform.TransformDirection(Vector3.down), out hit, 0.85f) || Physics.Raycast(transform.position + new Vector3(0, 0, 0.5f), transform.TransformDirection(Vector3.down), out hit, 0.85f) || Physics.Raycast(transform.position + new Vector3(0, 0, -0.5f), transform.TransformDirection(Vector3.down), out hit, 0.85f))
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump= false;
+        }
+
+    }
+
+
+
+
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        
+        if(ctx.performed)
+        {
+            deplacement = ctx.ReadValue<Vector3>();
+        }
+
+        
+    }
+
+    public void Jump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed && canJump)
+        {
+            rb.AddForce(new Vector3(0, jumpForce*100*(20-speed), 0));
+
+            canJump = false;
+        }
+    }
+
+    //public void Sprint(InputAction.CallbackContext ctx)
+    //{
+    //    if (ctx.performed && canJump)
+    //    {;
+    //        speed = speedSprint;
+    //    }
+    //    else if (ctx.canceled)
+    //    {
+    //        speed = speedNormal;
+    //    }
+    //}
+
+
+}
